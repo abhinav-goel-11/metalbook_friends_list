00071aaabea83ad3af959cb898c1a53f6d319566
abhinav <abhinav@hood.live>
2025-03-20 14:45:34 +0530

    pagination ui

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -1,10 +1,7 @@
 import { cn } from "@/lib/utils";
-import {
-  //   PaginationItem,
-  PaginationLink,
-  PaginationNext,
-  PaginationPrevious,
-} from "./ui/pagination";
+import Link from "next/link";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import { Button } from "./ui/button";
 
 type PaginationProps = {
   currentPage: number;
@@ -20,26 +17,46 @@ export default function Pagination({
   const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
   return (
     <div className="flex items-center justify-center space-x-2 mt-4">
-      <PaginationPrevious
+      <Link
         href="#"
         onClick={() => onPageChange(Math.max(1, currentPage - 1))}
-        className={cn("", { "cursor-not-allowed": currentPage === 1 })}
-      />
-      {pages.map((page) => (
-        <PaginationLink
-          href="#"
-          isActive={currentPage === page}
-          onClick={() => onPageChange(page)}
-          key={page}
-        >
-          {page}
-        </PaginationLink>
-      ))}
-      <PaginationNext
+        className={cn(
+          "flex items-center gap-2 border border-gray-500 p-1 rounded-lg",
+          {
+            "cursor-not-allowed": currentPage === 1,
+          }
+        )}
+      >
+        <ChevronLeft />
+        <span className="hidden sm:inline">Previous</span>
+      </Link>
+      <div className="flex items-center gap-2">
+        {pages.map((page) => (
+          <Button
+            onClick={() => onPageChange(page)}
+            className={cn("text-black bg-white hover:bg-white/50 shadow", {
+              "bg-black text-white hover:bg-black": currentPage === page,
+            })}
+            key={page}
+          >
+            {page}
+          </Button>
+        ))}
+      </div>
+
+      <Link
         href="#"
         onClick={() => onPageChange(Math.min(totalPages, currentPage + 1))}
-        className={cn("", { "cursor-not-allowed": currentPage === totalPages })}
-      />
+        className={cn(
+          "flex items-center gap-2 border border-gray-500 p-1 rounded-lg",
+          {
+            "cursor-not-allowed": currentPage === totalPages,
+          }
+        )}
+      >
+        <span className="hidden sm:inline">Next</span>
+        <ChevronRight />
+      </Link>
     </div>
   );
 }
